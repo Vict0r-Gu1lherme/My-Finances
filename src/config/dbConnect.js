b21cbee8b39f82232ad0c98b57c91de051fc44eb
Victor Guilherme <victor.guilherme.040301@gmail.com>
2022-11-11 14:30:05 -0300

    config bd

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://MyFinances:sKc2E1T6J8TmOk6C@myfinances.uk6ymqe.mongodb.net/My-Finances");
+const stringDeConexao = ""
+
+mongoose.connect(stringDeConexao);
 
 let db = mongoose.connection;
 
